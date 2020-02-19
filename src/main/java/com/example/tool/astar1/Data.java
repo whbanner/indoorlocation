266be794299384ee15ad3bce116b9fbdefdc5af1
whbanner <china_wh@163.com>
2020-02-19 23:10:38 +0800

    A星1

---
+++ src/main/java/com/example/tool/astar1/Data.java
@@ -0,0 +1,23 @@
+package com.example.tool.astar1;
+
+
+public class Data
+{
+	Point point;
+	double g;
+	double h;
+	Data parent;
+	
+	public Data(Point p, double g, double h, Data parent)
+	{
+		this.point = p;
+		this.g = g;
+		this.h = h;
+		this.parent = parent;
+	}
+	
+	double f()
+	{
+		return g + h;
+	}
+}
